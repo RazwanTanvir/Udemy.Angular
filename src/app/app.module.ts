185267b86e6b51d2695d1dbc4e1895d80b200387
Razwan Ahmed Tanvir <razwantanvir8050@gmail.com>
2021-03-22 01:49:05 +0600

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import {AlertComponent} from './alert/alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent,
+    AlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
